1eadbd0b94020c33905acf6aa0890ca89f8b4cd7
“Dale <dalespence03@gmail.com>
2024-05-16 18:58:27 -0700

    pushing map

--- godot projects/Scripts/Player.gd
+++ godot projects/Scripts/Player.gd
@@ -57,7 +57,7 @@ func _physics_process(delta):
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var input_dir = Input.get_vector("left", "right", "up", "down") # add scroll up and scroll down later 
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
@@ -86,3 +86,7 @@ func _headbob(time) -> Vector3:
 	pos.y = sin(time * BOB_FREQ) * BOB_AMP
 	pos.x = cos(time * BOB_FREQ /2) * BOB_AMP
 	return pos
+
+
+func _on_texture_rect_resized():
+	pass # Replace with function body.
